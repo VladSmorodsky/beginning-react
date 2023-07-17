9f88cbec98177d805d2fd5ab4eee680283847245
Vlad Smorodsky <smorodsky.odesa@gmail.com>
2023-07-17 22:31:17 +0300

    Lesson 11: used fetch api

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,15 +1,46 @@
 import Layout from "./Layout";
+import {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 
 export default function Blog() {
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('https://www.reddit.com/r/aww.json')
+      .then(response => response.json())
+      .then(result => {
+        setIsLoading(false);
+        setPosts(result.data.children);
+      })
+      .catch(error => {
+        setIsLoading(false);
+        setError(error);
+      });
+  }, []);
   return (
     <Layout>
-      <div>
-        <ul>
-          <li><Link to={'/blog/1'}>Post 1</Link></li>
-          <li><Link to={'/blog/2'}>Post 2</Link></li>
-        </ul>
-      </div>
+
+      {isLoading && (
+        <p>Loading...</p>
+      )}
+
+      {error && (
+        <p>There is an error</p>
+      )}
+
+      {posts && (
+        <div>
+          <ul>
+            {posts.map(post => (
+              <li key={post.data.id}>
+                <Link to={`/blog/${post.data.id}`}>{post.data.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </Layout>
   );
 }
